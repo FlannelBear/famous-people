a6b1ff8a3de072a3c08aac6823b089303ab7612c
Flannel Bear <josh.flannelbear@gmail.com>
2018-06-20 10:04:48 -0500

    create Header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class Header extends Component{
+   constructor(){
+      super();
+      
+   }
+
+   render(){
+      return (
+      <header className="App-header">
+         <h1 className="App-title">My Header</h1>
+       </header>
+      );
+   }
+}
+
+export default Header;
